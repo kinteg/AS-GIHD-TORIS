f6892d235cf614d5b455da9340ce9352c42af96b
nikita <nik.veko@yandex.ru>
2020-01-29 14:59:30 +0300

    test

---
+++ src/test/java/ru/iac/ASGIHDTORIS/AsGihdTorisApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.iac.ASGIHDTORIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsGihdTorisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

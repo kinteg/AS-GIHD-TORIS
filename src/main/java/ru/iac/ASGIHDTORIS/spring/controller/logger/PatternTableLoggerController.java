ae6bb8ffca780225e7e3085074ce7d0ad14c8fed
nikita <nik.veko@yandex.ru>
2020-04-09 16:37:53 +0300

    Кучу всего по патернам

--- src/main/java/ru/iac/ASGIHDTORIS/spring/controller/logger/PatternTableLoggerController.java
+++ src/main/java/ru/iac/ASGIHDTORIS/spring/controller/logger/PatternTableLoggerController.java
@@ -5,10 +5,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.iac.ASGIHDTORIS.spring.domain.*;
 import ru.iac.ASGIHDTORIS.spring.repo.BeforeAfterPatternRepo;
 import ru.iac.ASGIHDTORIS.spring.repo.BeforeAfterPatternTableRepo;
@@ -34,7 +31,7 @@ public class PatternTableLoggerController {
         return patternLoggerRepo.findById((long) id);
     }
 
-    @GetMapping("/getAll")
+    @PostMapping("/getAll")
     public Page<PatternTableLogger> getAllLogger(@PageableDefault Pageable pageable) {
         return patternLoggerRepo.findAll(pageable);
     }

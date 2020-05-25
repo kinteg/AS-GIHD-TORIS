c51c0a5a4b1f86083bcdbee75819f6fecef010b1
nikita <nik.veko@yandex.ru>
2020-05-25 15:12:12 +0300

    Авторизация

--- src/main/java/ru/iac/ASGIHDTORIS/spring/controller/UserController.java
+++ src/main/java/ru/iac/ASGIHDTORIS/spring/controller/UserController.java
@@ -1,16 +1,18 @@
 package ru.iac.ASGIHDTORIS.spring.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-@RequestMapping("api/source/")
+@Slf4j
+@RequestMapping("api/user/")
 @RestController
 public class UserController {
 
     @GetMapping("/acceptToken/{token}")
     public boolean checkName(@PathVariable String token) {
+        log.info(token);
         return true;
     }
 
